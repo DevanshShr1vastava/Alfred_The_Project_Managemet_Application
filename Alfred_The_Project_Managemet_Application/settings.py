0b1832a58403cbe4c117b9c60bef56e28a0ca947
Devansh Shrivastava <devansh.shrivastava55@gmail.com>
2024-11-11 11:05:49 +0530

    update settings

--- Alfred_The_Project_Managemet_Application/settings.py
+++ Alfred_The_Project_Managemet_Application/settings.py
@@ -133,9 +133,6 @@ STATIC_URL = '/static/'
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
-    STATICFILES_DIRS = (
-        os.path.join(BASE_DIR,'templates/assets'),
-    )
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
